50928eb891a3591cb6036fc276a670e2952bb0b4
vanshagarwal20 <vanshagarwal3329@gmail.com>
2026-01-13 16:03:32 +0530

    [vansh] add file

---
+++ add.java
@@ -0,0 +1,10 @@
+
+public class add { 
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        int d = 2 * a + b;
+        System.out.println("Sum: " + d);
+    }
+}
