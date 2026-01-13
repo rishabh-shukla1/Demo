c1bee7e0f5c11e6fd2f2f273a5fe76eb464359b3
Harshita Agrawal <harshiagrawal3459@gmail.com>
2026-01-13 16:02:51 +0530

    added add.java

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int sum = a + b + c;
+    public static void main(String[] args) {
+        add obj = new add();
+        System.out.println("The sum is: " + obj.sum);
+    }
+
+}
